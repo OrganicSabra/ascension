80ee3788c4723f17e34f4143a90369fa966c49ed
Sabra Duffiney <sabraduffiney@gmail.com>
2015-10-23 15:18:54 -0500

    pdf

--- wp-content/themes/ascension/sections/full-width.php
+++ wp-content/themes/ascension/sections/full-width.php
@@ -1,10 +1,31 @@
-<?php function fullWidth($row,$ID) { ?>
+<?php function fullWidth($row,$ID) { 
+	$bgColor = $row['background_color'];
+	$bgImage = $row['background_image'];
+	$fontColor = $row['font_color'];
+	$background = '';
+	if($bgColor) {
+		if($bgImage) {
+			$background = 'background: '.$bgColor.' url(\''.$bgImage.'\') no-repeat center center;';
+		}
+		else {
+			$background = 'background-color: '.$bgColor.';';
+		}
+	}
+	
+	if($fontColor) {
+		$fontColor = 'color: '.$fontColor.';';
+	}	
+?>
 
-	<div class="row fullwidth">
+	<div class="row fullwidth" style="<?php echo $background . $fontColor; ?>">
 		<div class="widcon">
-			<?php
-			echo 'Full Width Section';
-			?>	
+			<?php if($row['section_title']) {
+				echo '<h1>'.$row['section_title'].'</h1>';
+			} ?>
+			<?php echo $row['content_1']; ?>
+			<?php if($row['section_title']) {
+				echo '<div class="bottom">'.$row['bottom_content'].'</div>';
+			} ?>
 		</div>
 	</div>
 
