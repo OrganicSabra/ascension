2dd47c07ee78bc06cff2884ffac5d6389bf8b8d8
Sabra Duffiney <sabraduffiney@gmail.com>
2015-10-22 23:48:17 -0500

    Theme Fix

--- wp-content/themes/ascension/page_home.php
+++ wp-content/themes/ascension/page_home.php
@@ -8,7 +8,9 @@ get_header(); ?>
 	<div id="content" class="site-content" role="main">
 		<?php
 			while ( have_posts() ) : the_post();
-
+				?>
+				<h2><?php echo get_the_title(); ?></h2>
+				<?php
 				the_content();
 			
 			endwhile;
