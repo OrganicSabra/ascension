1e3ae80f0ef9d842a9201cccd97aac556b3ac577
Sabra Duffiney <sabraduffiney@gmail.com>
2015-10-23 15:17:17 -0500

    sd

--- wp-content/themes/ascension/sections/fifty-fifty.php
+++ wp-content/themes/ascension/sections/fifty-fifty.php
@@ -1,10 +1,37 @@
-<?php function fiftyFifty($row,$ID) { ?>
+<?php function fiftyFifty($row,$ID) { 
+	$bgColor = $row['background_color'];
+	$bgImage = $row['background_image'];
+	$fontColor = $row['font_color'];
+	$background = '';
+	if($bgColor) {
+		if($bgImage) {
+			$background = 'background: '.$bgColor.' url(\''.$bgImage.'\') no-repeat center center;';
+		}
+		else {
+			$background = 'background-color: '.$bgColor.';';
+		}
+	}
+	
+	if($fontColor) {
+		$fontColor = 'color: '.$fontColor.';';
+	}
+?>
 
-	<div class="row halves">
+	<div class="row halves" style="<?php echo $background . $fontColor; ?>">
 		<div class="widcon">
-			<?php
-			echo 'Half and Half Section';
-			?>
+			<?php if($row['section_title']) {
+				echo '<h1>'.$row['section_title'].'</h1>';
+			} ?>
+			<div class="half">
+				<?php echo $row['content_1']; ?>
+			</div>
+			<div class="half">
+				<?php echo $row['content_2']; ?>
+			</div>
+			<div class="clear"></div>
+			<?php if($row['section_title']) {
+				echo '<div class="bottom">'.$row['bottom_content'].'</div>';
+			} ?>
 		</div>	
 	</div>
 
