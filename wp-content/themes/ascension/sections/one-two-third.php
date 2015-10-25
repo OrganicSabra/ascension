76c9bc6e5ff112f0325af25ae51fd9900d02f7fb
Sabra Duffiney <sabraduffiney@gmail.com>
2015-10-25 16:37:21 -0500

    t

--- wp-content/themes/ascension/sections/one-two-third.php
+++ wp-content/themes/ascension/sections/one-two-third.php
@@ -7,6 +7,9 @@
 	$topPad = $row['top_padding'];
 	$botPad = $row['bottom_padding'];
 	
+	$oneVertAlign = $row['one_third_vertical_align'];
+	$twoVertAlign = $row['two_third_vertical_align'];
+	
 	if($bgColor) {
 		if($bgImage) {
 			$background = 'background: '.$bgColor.' url(\''.$bgImage.'\') no-repeat top center;';
@@ -31,6 +34,12 @@
 		$botPad = 'padding-bottom: '.$botPad.'px;';	
 	}	
 	
+	if($oneVertAlign) {
+		$oneVertAlign = 'vertical-align: '.$oneVertAlign.';';
+	}
+	if($twoVertAlign) {
+		$twoVertAlign = 'vertical-align: '.$twoVertAlign.';';
+	}
 ?>
 
 	<div class="row onetwothirds" style="<?php echo $background . $bgSize . $fontColor; ?>">
@@ -40,10 +49,10 @@
 			} ?>
 			<div class="onetwotable">
 				<div class="onetworow">
-					<div class="onethird">
+					<div class="onethird" style="<?php echo $oneVertAlign; ?>">
 						<?php echo $row['content_1']; ?>
 					</div>
-					<div class="twothird">
+					<div class="twothird" style="<?php echo $twoVertAlign; ?>">
 						<?php echo $row['content_2']; ?>
 					</div>
 				</div>
