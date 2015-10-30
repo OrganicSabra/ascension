e2023a2e58dada86d7edea76de1a3ff3a7a6adb7
Sabra Duffiney <sabraduffiney@gmail.com>
2015-10-29 20:20:21 -0500

    s

--- wp-content/themes/ascension/header.php
+++ wp-content/themes/ascension/header.php
@@ -22,6 +22,7 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
+	<script scr="/wp-content/themes/ascension/js/script.js" />
 </head>
 
 <body <?php body_class(); ?>>
