f8eb38bc2a0180dcdb0954234ba1a879ccc430ec
Sabra Duffiney <sabraduffiney@gmail.com>
2015-11-02 14:58:22 -0600

    w

--- wp-content/themes/ascension/functions.php
+++ wp-content/themes/ascension/functions.php
@@ -316,4 +316,152 @@ function btw_load_widget() {
 }
 add_action( 'widgets_init', 'btw_load_widget' );
 
+
+
+class carousel_widget extends WP_Widget {
+	
+	function __construct() {
+		parent::__construct(
+		'carousel_widget', 
+		__('Blue Title Widget', 'carousel_widget_domain'), 
+		array( 'description' => __( 'Widget with Blue Stripe behind Title', 'carousel_widget_domain' ), ) 
+		);
+	}
+	
+	// Widget Front-End
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$desc = apply_filters( 'widget_description', $instance['desc'] );
+		$image = apply_filters( 'widget_description', $instance['image'] );
+		$button = apply_filters( 'widget_description', $instance['button'] );
+		$link = apply_filters( 'widget_description', $instance['link'] );
+		$ctaimg = apply_filters( 'widget_description', $instance['ctaimg'] );
+		
+		if ( ! empty( $image ) ) 
+			$hasImageClass = ' hasImage';
+		
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		
+		echo '<div class="blue-title-widget'.$hasImageClass.'">';
+		
+		if ( ! empty( $title ) )
+		echo '<div class="blue-banner"><h4>' . $title . '</h4></div>';
+		
+		if ( ! empty( $image ) ) 
+			echo '<div class="content">';
+		
+		if ( ! empty( $desc ) )
+		echo '<p>' . $desc . '</p>';
+		
+		echo '<div class="cta">';
+		
+		if ( ! empty( $button ) )
+		echo '<a href="'.$link.'" title="'.$title.'">' . $button . '</a>';
+		
+		if ( ! empty( $ctaimg ) )
+		echo '<img src="' . $ctaimg . '" alt="'.$title.'" />';
+		
+		echo '</div>';
+		
+		if ( ! empty( $image ) ) 
+			echo '</div>';
+		
+		if ( ! empty( $image ) )
+		echo '<img src="' . $image . '" alt="'.$title.'" class="absimg" />';
+		
+		echo '</div>';
+		
+		echo $args['after_widget'];
+	}
+			
+	// Widget Backend 
+	public function form( $instance ) {
+	if ( isset( $instance[ 'title' ] ) ) {
+		$title = $instance[ 'title' ];
+	}
+	else {
+		$title = __( 'New Title', 'carousel_widget_domain' );
+	}
+	
+	if ( isset( $instance[ 'desc' ] ) ) {
+		$desc = $instance[ 'desc' ];
+	}
+	else {
+		$desc = __( 'New Description', 'carousel_widget_domain' );
+	}
+
+	if ( isset( $instance[ 'image' ] ) ) {
+		$image = $instance[ 'image' ];
+	}
+	else {
+		$image = __( 'New Image URL', 'carousel_widget_domain' );
+	}
+
+	if ( isset( $instance[ 'button' ] ) ) {
+		$button = $instance[ 'button' ];
+	}
+	else {
+		$button = __( 'Button Text', 'carousel_widget_domain' );
+	}
+	
+	if ( isset( $instance[ 'link' ] ) ) {
+		$link = $instance[ 'link' ];
+	}
+	else {
+		$link = __( 'Button Link', 'carousel_widget_domain' );
+	}
+	
+	if ( isset( $instance[ 'ctaimg' ] ) ) {
+		$ctaimg = $instance[ 'ctaimg' ];
+	}
+	else {
+		$ctaImg = __( 'CTA Image', 'carousel_widget_domain' );
+	}
+	// Widget admin form
+	?>
+	<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+	</p>
+	<p>
+		<label for="<?php echo $this->get_field_id( 'desc' ); ?>"><?php _e( 'Description:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'desc' ); ?>" name="<?php echo $this->get_field_name( 'desc' ); ?>" type="text" value="<?php echo esc_attr( $desc ); ?>" />
+	</p>
+	<p>
+		<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php _e( 'Image URL:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" type="text" value="<?php echo esc_attr( $image ); ?>" />
+	</p>
+	<p>
+		<label for="<?php echo $this->get_field_id( 'button' ); ?>"><?php _e( 'Button Text:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'button' ); ?>" name="<?php echo $this->get_field_name( 'button' ); ?>" type="text" value="<?php echo esc_attr( $button ); ?>" />
+	</p>
+	<p>
+		<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Button Link:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_attr( $link ); ?>" />
+	</p>
+	<p>
+		<label for="<?php echo $this->get_field_id( 'ctaimg' ); ?>"><?php _e( 'CTA Image:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'ctaimg' ); ?>" name="<?php echo $this->get_field_name( 'ctaimg' ); ?>" type="text" value="<?php echo esc_attr( $ctaimg ); ?>" />
+	</p>
+	<?php 
+	}
+		
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['desc'] = ( ! empty( $new_instance['desc'] ) ) ? strip_tags( $new_instance['desc'] ) : '';
+		$instance['image'] = ( ! empty( $new_instance['image'] ) ) ? strip_tags( $new_instance['image'] ) : '';
+		$instance['button'] = ( ! empty( $new_instance['button'] ) ) ? strip_tags( $new_instance['button'] ) : '';
+		$instance['link'] = ( ! empty( $new_instance['link'] ) ) ? strip_tags( $new_instance['link'] ) : '';
+		$instance['ctaimg'] = ( ! empty( $new_instance['ctaimg'] ) ) ? strip_tags( $new_instance['ctaimg'] ) : '';
+		return $instance;
+	}
+}
+
+function carousel_load_widget() {
+	register_widget( 'carousel_widget' );
+}
+add_action( 'widgets_init', 'carousel_load_widget' );
+
 ?>
