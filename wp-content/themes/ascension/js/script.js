6b1fdee13e5ff6eab56d1a191022018ffdcbb515
Sabra Duffiney <sabraduffiney@gmail.com>
2015-11-08 14:53:58 -0600

    sc

--- wp-content/themes/ascension/js/script.js
+++ wp-content/themes/ascension/js/script.js
@@ -1,8 +1,8 @@
 jQuery(function( $ ) {
 	
-	$('.main-navigation ul li').hover() {
+	$('.main-navigation ul li').hover(function() {
 		$(this).children('ul').slideToggle();
-	}
+	});
 	
 	$('.mobile-btn').click(function() {
 		$('.main-menu').slideToggle();
